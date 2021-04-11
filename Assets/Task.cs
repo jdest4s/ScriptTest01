60b3fe3ddeda98fdf136028fd4e86d8a54063a72
jdest4s <jdest4s@yahoo.co.jp>
2021-04-11 17:26:55 +0900

    余分な文を削除
    タイトル通り＆練習

--- Assets/Task.cs
+++ Assets/Task.cs
@@ -4,28 +4,22 @@ using UnityEngine;
 
 public class Task : MonoBehaviour
 {
-
-    // Use this for initialization
     void Start()
     {
-        // 要素数5の配列を初期化する
         int[] points = new int[5];
 
-        // 配列の各要素に値を代入する
         points[0] = 10;
         points[1] = 20;
         points[2] = 30;
         points[3] = 40;
         points[4] = 50;
 
-        // 配列の要素をすべて表示する
         for (int i = 0; i < 5; i++)
         {
             Debug.Log(points[i]);
         }
 
         {
-            // 配列の要素をすべて表示する
             for (int i = 4; i >= 0; i--)
             {
                 Debug.Log(points[i]);
@@ -33,7 +27,6 @@ public class Task : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
 
