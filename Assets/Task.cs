82b2399068e385d61dfdce3eebbc52ad8b827ee1
jdest4s <jdest4s@yahoo.co.jp>
2021-04-11 17:20:06 +0900

    Initial commit

---
+++ Assets/Task.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        // 要素数5の配列を初期化する
+        int[] points = new int[5];
+
+        // 配列の各要素に値を代入する
+        points[0] = 10;
+        points[1] = 20;
+        points[2] = 30;
+        points[3] = 40;
+        points[4] = 50;
+
+        // 配列の要素をすべて表示する
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(points[i]);
+        }
+
+        {
+            // 配列の要素をすべて表示する
+            for (int i = 4; i >= 0; i--)
+            {
+                Debug.Log(points[i]);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
